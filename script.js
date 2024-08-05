de329829671632172fff826d082000877181afdc
Anahilach <anahibt_88@hotmail.com>
2024-08-05 16:17:03 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+script.js
+const fulImgBox = document.getElementById("fulImgBox"),
+fulimg = document.getElementById("fulImg");
+
+function openfulImg(reference){
+	fulimgbox.style.display = "flex"
+	fulImg.src = reference
+}
+function closeImg(){
+	fulImgBox.style.display = "none";
+}
